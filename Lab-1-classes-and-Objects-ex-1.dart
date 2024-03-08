94dd925a156534395fdd79f28e6940b399d89a64
Sofonyas <sofstzz@gmail.com>
2024-03-08 15:49:54 +0300

    ex-1

---
+++ Lab-1-classes-and-Objects-ex-1.dart
@@ -0,0 +1,26 @@
+class Person {
+  String name;
+  int age;
+  String address;
+  
+  Person(this.name, this.age, this.address);
+
+  void printObject() {
+    print("Name: $name");
+    print("Age: $age");
+    print("Address: $address");
+  }
+}
+
+void main() {  
+  Person person1 = Person("John", 30, "123 Main St");
+  
+  print("Initial Object:");
+  person1.printObject();
+  
+  person1.age = 35;
+  person1.address = "456 Elm St";
+
+  print("Modified Object:");
+  person1.printObject();
+}
