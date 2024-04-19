1c04f7f09465c8dea5b267ed9f69494a1d3da904
Sofonyas <sofstzz@gmail.com>
2024-04-19 16:07:34 +0300

    provider

--- demo_2/Lab-1-Control-Flow-ex-1.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  int number = 7;
-
-  if (number % 2 == 0) {
-    print('$number is even.');
-  } else {
-    print('$number is odd.');
-  }
-}
--- demo_2/Lab-1-Variables-and-Data-types-ex-1.dart
+++
@@ -1,7 +0,0 @@
-void main(){
-  String name = "Sofonyas";
-  int age = 21;
-  String favroutieColor = "aqua";
-
-  print("My name is $name. I am $age years old. And my favourite color is $favroutieColor.");
-}
